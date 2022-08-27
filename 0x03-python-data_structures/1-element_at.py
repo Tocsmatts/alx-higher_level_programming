f12d18d465917df682dcf413183be789f0efd81a
Tocsmatts <toochukwuezeobi@gmail.com>
2022-08-27 14:48:51 -0700

    Updating file

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,7 +3,10 @@
 def element_at(my_list, idx):
     if idx < 0:
         return None
-    elif idx > len(my_list):
+
+    length = len(my_list) - 1
+
+    if idx > length:
         return None
-    else:
-        return my_list[idx]
+
+    return my_list[idx]
