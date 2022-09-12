1359e384ca279047b77f6c294d72508940094a92
Tocsmatts <toochukwuezeobi@gmail.com>
2022-09-12 11:42:14 -0700

    Adding files for task 0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    numcount = 0
+
+    for a in range(x):
+        try:
+            print("{}".format(my_list[a]), end="")
+        except:
+            break
+        else:
+            numcount += 1
+
+    print()
+    return(numcount)
