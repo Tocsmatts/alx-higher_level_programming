b53f379dbb299ad472dbf16b8150ff9c7224f2ce
Tocsmatts <toochukwuezeobi@gmail.com>
2022-09-26 14:47:07 -0700

    Adding file for task 2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+""" This is the file doc module """
+
+
+def is_same_class(obj, a_class):
+    """ Returns True or False to check inheritance """
+    return type(obj) is a_class
