affb3a563a6a2799d703c6fbc77b97cc72bf829d
Tocsmatts <toochukwuezeobi@gmail.com>
2022-09-26 16:27:04 -0700

    Adding file for task 8

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+""" The file module doc """
+
+
+class Rectangle:
+    """ class created """
+
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
