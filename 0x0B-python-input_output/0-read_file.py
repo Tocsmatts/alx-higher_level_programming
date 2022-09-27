843ba05fddf65ea5eca36f0b4f576549fb4ba8f4
Tocsmatts <toochukwuezeobi@gmail.com>
2022-09-27 11:40:46 -0700

    Updating file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,4 +7,5 @@ def read_file(filename=""):
     """ function reads text file """
 
     with open(filename, encoding="utf-8") as myFile:
-        print(myFile.read())
+        x = myFile.read()
+        print(x, end='')
