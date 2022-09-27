4e0f9b518c811cd18734c9d37eed63557e8a7b36
Tocsmatts <toochukwuezeobi@gmail.com>
2022-09-26 17:05:19 -0700

    Adding file

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -5,6 +5,7 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
+    """ This is a class """
 
     def __init__(self, size):
         self.integer_validator("size", size)
