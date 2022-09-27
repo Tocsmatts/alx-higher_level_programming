02bcbd3421484b1fbcfdb17dcfcf7cd9099a1a48
Tocsmatts <toochukwuezeobi@gmail.com>
2022-09-27 12:29:10 -0700

    Adding file for task 3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Program returns JSON representation """
+
+
+import json
+
+def to_json_string(my_obj):
+    """ function returns the JSON representation """
+
+    return json.dumps(my_obj)
