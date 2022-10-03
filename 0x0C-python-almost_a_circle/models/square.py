4029f1a769316de8c327294a776753eec3cd3ddc
Tocsmatts <toochukwuezeobi@gmail.com>
2022-10-03 13:44:03 -0700

    Adding file square.py

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" The module for class Square """
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """class Rectangle """
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """ Initializations """
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """ str """
+        sq = "[Square]"
+        sq_id = " ({}) ".format(self.id)
+        sq_xy = "{}/{} - ".format(self.x, self.y)
+        sq_sz = "{}/{}".format(self.width, self.height)
+
+        return sq + sq_id + sq_xy + sq_sz
