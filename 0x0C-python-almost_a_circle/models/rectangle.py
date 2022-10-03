eb4c3f87c1199e16fd3e5867923e7095884b0230
Tocsmatts <toochukwuezeobi@gmail.com>
2022-10-03 08:38:00 -0700

    Updating file for task 4

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python3
+""" The module for rectangle """
+from models.base import Base
+
+
+class Rectangle(Base):
+    """ class Rectangle inherites Base """
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """ Initializing """
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
+
+    @property
+    def width(self):
+        """ Getter """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ Setter """
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """ Getter """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ Setter """
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
+
+    @property
+    def x(self):
+        """ Getter """
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """ Setter """
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= than 0")
+        self.__x = value
+
+    @property
+    def y(self):
+        """ Getter """
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """ Setter """
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= than 0")
+        self.__y = value
+
+    def area(self):
+        return self.__width * self.__height
