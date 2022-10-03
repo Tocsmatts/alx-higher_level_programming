8034f215d86d82e9457cd6fb378c7cce4db68fb4
Tocsmatts <toochukwuezeobi@gmail.com>
2022-10-03 14:07:34 -0700

    Updating file for task 12

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -29,3 +29,21 @@ class Square(Rectangle):
         """ Setter """
         self.width = value
         self.height = value
+
+    def update(self, *args, **kwargs):
+        """ update method """
+        if args is not None and len(args) != 0:
+            list_atr = ['id', 'size', 'x', 'y']
+            for i in range(len(args)):
+                if list_atr[i] == 'size':
+                    setattr(self, 'width', args[i])
+                    setattr(self, 'height', args[i])
+                else:
+                    setattr(self, list_atr[i], args[i])
+        else:
+            for key, value in kwargs.items():
+                if key == 'size':
+                    setattr(self, 'width', value)
+                    setattr(self, 'height', value)
+                else:
+                    setattr(self, key, value)
